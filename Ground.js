7af93d0c536820645e1dab451c842b3d4116d94d
saikat2007 <67913259+saikat2007@users.noreply.github.com>
2020-11-17 12:08:26 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -1,17 +1,31 @@
-class Ground {
-    constructor(x,y,width,height) {
-      var options = {
-          isStatic: true
-      }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
+class Divisions {
+     constructor{X,y,w,h} {
+         var options = {
+
+            isStatic:true 
+            
+
+
+         }
+         this.body = Bodies.rectangle(x,y,w,h,options);
+         this.w = w;
+         this.w = w;
+         world.add{world,this.body};
+        }
+        display(){
+            var pos = this.body.position;
+            rectMode(CENTER);
+            fill("white");
+            rect(pos.x,pos.y,this.w,this.h);
+        }
+    };
+    
+    for (var j = 0; j < particles.length; j++){
+
+        particles[j].display();
     }
-    display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill("brown");
-      rect(pos.x, pos.y, this.width, this.height);
+    for(var k = 0; k < display.length;k++){
+
+        divisions[k].display();
     }
-  };
+    
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AngryBirdsStage7
+# p5.play-boilerplate
+Boiler plate for p5.play
